7ab560da859ff6b8d46e818809ad264416c79480
miguelrosa81 <miguelrosa81@gmail.com>
2023-11-11 20:15:09 -0300

    dev1

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,4 +5,4 @@ const app = express();
 
 app.get('/', (request, response) => response.status(200).send('Olá Mundo!'))
 
-app.listen(3333, () => console.log('porta 3333'));
+app.listen(8888, () => console.log('porta 3333'));
